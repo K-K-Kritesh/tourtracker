1e099771da02d96006d1414b653a5103d76c62ef
kabariya kritesh <kabariyakritesh65@gmail.com>
2021-05-14 22:24:33 +0530

    Table Created

--- Connection.py
+++ Connection.py
@@ -1,5 +1,6 @@
 import mysql.connector as mysql
 from mysql.connector import Error
+import SqlQuery as query
 
 class Connection:
     def __init__(self):
@@ -13,11 +14,25 @@ class Connection:
             if connection.is_connected():
                 cur = connection.cursor()
                 self.createDataBase(cur)
+                self.Create_Table_tour_people_register(cur)
+                self.Create_Table_trip_detail(cur)
+                #self.get_Field(cur, query.TOUR_PEOPLE_REGISTER)
+                self.get_Field(cur, query.TRIP_DETAIL)
         except Error as e:
             print("Error while coonecting error",e)
 
     def createDataBase(self,cur):
-        cur.execute("CREATE DATABASE IF NOT EXISTS tourtracker")
+        cur.execute(query.CREATE_DATABASE)
+
+    def Create_Table_tour_people_register(self, cur):
+        cur.execute(query.CREATE_TABLE_TOUR_PEOPLE_REGISTER)
+
+    def Create_Table_trip_detail(self, cur):
+        cur.execute(query.CREATE_TABLE_TRIP_DETAIL)
+
+    def get_Field(self, cur, tablename):
+        cur.execute(f"select * from {tablename}")
+        print([x[0] for x in cur.description])
 
 
 
--- SqlQuery.py
+++ SqlQuery.py
@@ -1,3 +1,11 @@
-class SqlQuery:
-    def __init__(self):
-        pass
+TOUR_PEOPLE_REGISTER = "tourPeopleRegister"
+TRIP_DETAIL = "tripdetail"
+PLACE_DETAIL = "placedetail"
+TRIP_CODE = "tripcodes"
+TRIP_MEMBERS = "tripmembers"
+TRIP_LOCATION = "triplocation"
+
+
+CREATE_DATABASE = "CREATE DATABASE IF NOT EXISTS tourtracker"
+CREATE_TABLE_TOUR_PEOPLE_REGISTER = f"CREATE TABLE IF NOT EXISTS {TOUR_PEOPLE_REGISTER} (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, first_name VARCHAR(255),last_name VARCHAR(255),email_id VARCHAR(255),mobile_no VARCHAR(255),gender INT(1),address TEXT,password VARCHAR(255),trusted_person_t_code VARCHAR(255),created_trips VARCHAR(255),joined_trips VARCHAR(255),trusted_person VARCHAR(255),birth_date DATETIME,blood_group VARCHAR(255),health_problems VARCHAR(255))"
+CREATE_TABLE_TRIP_DETAIL = f"CREATE TABLE IF NOT EXISTS {TRIP_DETAIL} (trip_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, trip_title VARCHAR(255), sorce_location VARCHAR(255), destination_location VARCHAR(255), start_time_date DATETIME, end_time_date DATETIME, no_of_place_visit INT(2))"
